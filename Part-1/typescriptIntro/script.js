9dc128feef8b7b455b81f34ff465e750e9f5cffe
Varun Kaushik <varunkaushik@Varuns-MBP.fritz.box>
2024-10-06 21:38:25 +0200

    added basic function to ts

--- Part-1/typescriptIntro/script.js
+++ Part-1/typescriptIntro/script.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('hey bro :)');
+function add(arg1, arg2) {
+    return arg1 + arg2;
+}
+function concatenate(arg1, arg2) {
+    return arg1 + arg2;
+}
+const a1 = 2;
+const a2 = 3;
+const r = add(a1, a2);
+console.log(r);
+console.log(typeof r);
+const a4 = 'varun';
+const a5 = ' kaushik';
+console.log(concatenate(a4, a5));
--- Part-1/typescriptIntro/script.ts
+++ Part-1/typescriptIntro/script.ts
@@ -1,1 +1,18 @@
-console.log('hey bro :)');
+function add(arg1: number, arg2: number) {
+    return arg1 + arg2;
+}
+
+function concatenate(arg1: string, arg2: string) {
+    return arg1 + arg2;
+}
+
+const a1 = 2;
+const a2 = 3;
+const r = add(a1, a2);
+
+console.log(r);
+console.log(typeof r);
+
+const a4 = 'varun';
+const a5 = ' kaushik';
+console.log(concatenate(a4, a5));
